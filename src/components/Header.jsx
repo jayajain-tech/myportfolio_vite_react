a51c03d2852efae9b6743db5506f72f65437c05d
Jaya Prabha Bhura <jayaprabha.bhura@team.telstra.com>
2025-12-31 16:48:43 +0530

    Add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from './NavBar';
+export default function Header() {
+    return (
+        <header className="header">
+            <h1>My Portfolio</h1>
+            <NavBar />
+        </header>
+    );
+}
